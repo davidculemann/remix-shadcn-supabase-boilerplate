95f3c999d762a11510c7350fe284c9cc5dc74166
davidculemann <davidculemann@gmail.com>
2024-09-17 21:42:03 +0100

    fix layout issues

--- app/routes/_auth.signin.tsx
+++ app/routes/_auth.signin.tsx
@@ -32,10 +32,8 @@ export default function Signin() {
 			<div className="flex items-center justify-center py-12">
 				<div className="mx-auto grid w-[350px] gap-6">
 					<div className="grid gap-2 text-center">
-						<h1 className="text-3xl font-bold">Login</h1>
-						<p className="text-balance text-muted-foreground">
-							Enter your email below to login to your account
-						</p>
+						<h1 className="text-3xl font-bold">Welcome back</h1>
+						<p className="text-balance text-muted-foreground">Enter your email and password</p>
 					</div>
 					<div className="grid gap-4">
 						<div className="grid gap-2">
@@ -54,6 +52,14 @@ export default function Signin() {
 						<LoadingButton className="w-full" loading={navigation.state === "submitting"}>
 							Sign in
 						</LoadingButton>
+						<div className="relative">
+							<div className="absolute inset-0 flex items-center">
+								<span className="w-full border-t" />
+							</div>
+							<div className="relative flex justify-center text-xs uppercase">
+								<span className="bg-background px-2 text-muted-foreground">Or continue with</span>
+							</div>
+						</div>
 						<ProviderLoginButton provider="google" />
 						<ProviderLoginButton provider="github" />
 					</div>
@@ -65,7 +71,7 @@ export default function Signin() {
 					</div>
 				</div>
 			</div>
-			<div className="hidden bg-muted lg:block">
+			<div className="hidden bg-muted lg:grid lg:place-items-center">
 				<Icons.logo className="object-cover dark:brightness-[0.2] dark:grayscale m-auto" />
 			</div>
 		</Form>
--- app/routes/_auth.tsx
+++ app/routes/_auth.tsx
@@ -2,7 +2,7 @@ import { Outlet } from "@remix-run/react";
 
 export default function AuthLayout() {
 	return (
-		<main className="h-full">
+		<main className="h-full px-8 sm:px-0">
 			<Outlet />
 		</main>
 	);
