2cae285d5ac3f02a55711ef787e02c608800fe37
davidculemann <davidculemann@gmail.com>
2024-09-24 21:58:11 +0100

    fix dark: selector in tailwind.config.cjs

--- app/routes/_landing._index.tsx
+++ app/routes/_landing._index.tsx
@@ -57,7 +57,7 @@ export default function Index() {
 
 				<section
 					id="features"
-					className="container space-y-6 bg-slate-50 py-8 --theme0d:bg-transparent md:py-12 lg:py-24"
+					className="container space-y-6 bg-slate-50 py-8 dark:bg-transparent md:py-12 lg:py-24"
 				>
 					<div className="mx-auto flex max-w-[58rem] flex-col items-center space-y-4 text-center dark:mt-10">
 						<h2 className="font-heading text-3xl leading-[1.1] sm:text-3xl md:text-6xl">
