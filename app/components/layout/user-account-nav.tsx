ad3ac9cc5f9bbd73a5ed3f38b97a4c0dc1c22603
davidculemann <davidculemann@gmail.com>
2024-10-06 18:46:23 +0100

    fix avatar

--- app/components/layout/user-account-nav.tsx
+++ app/components/layout/user-account-nav.tsx
@@ -14,7 +14,7 @@ export function UserAccountNav({ user }: { user: UserMetadata }) {
 	return (
 		<DropdownMenu>
 			<DropdownMenuTrigger>
-				<UserAvatar user={{ name: user.name || null, image: user.image || null }} className="h-8 w-8" />
+				<UserAvatar user={{ name: user.name || null, image: user.avatar_url || null }} className="h-8 w-8" />
 			</DropdownMenuTrigger>
 			<DropdownMenuContent align="end">
 				<div className="flex items-center justify-start gap-2 p-2">
