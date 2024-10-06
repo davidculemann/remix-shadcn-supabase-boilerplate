5bd51c614a45bf36566016259f74ad1d1f0ef76a
davidculemann <davidculemann@gmail.com>
2024-10-06 22:29:49 +0100

    bring in items to the header right

--- app/components/layout/navbar.tsx
+++ app/components/layout/navbar.tsx
@@ -1,12 +1,17 @@
+import type { SupabaseOutletContext } from "@/lib/supabase/supabase";
 import { ThemeToggle } from "@/routes/resources.theme-toggle";
+import { useOutletContext } from "@remix-run/react";
 import { SheetMenu } from "./sheet-menu";
-import { UserNav } from "./user-nav";
+import { UserAccountNav } from "./user-account-nav";
 
 interface NavbarProps {
 	title: string;
 }
 
 export function Navbar({ title }: NavbarProps) {
+	const { user } = useOutletContext<SupabaseOutletContext>();
+	const userMetaData = user?.user_metadata;
+
 	return (
 		<header className="sticky top-0 z-10 w-full bg-background/95 shadow backdrop-blur supports-[backdrop-filter]:bg-background/60 dark:shadow-secondary">
 			<div className="mx-4 sm:mx-8 flex h-14 items-center">
@@ -14,10 +19,10 @@ export function Navbar({ title }: NavbarProps) {
 					<SheetMenu />
 					<h1 className="font-bold">{title}</h1>
 				</div>
-				<div className="flex flex-1 items-center justify-end">
+				<span className="gap-4 flex flex-1 items-center justify-end">
 					<ThemeToggle />
-					<UserNav />
-				</div>
+					{userMetaData && <UserAccountNav user={userMetaData} />}
+				</span>
 			</div>
 		</header>
 	);
