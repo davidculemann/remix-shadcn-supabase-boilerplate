a572cd8fe1daf07d9e6d8548b5c203a879b4e31f
davidculemann <davidculemann@gmail.com>
2024-10-21 22:02:31 +0100

    change name to corpify

--- app/config/site.ts
+++ app/config/site.ts
@@ -2,14 +2,14 @@ import type { SiteConfig } from "../lib/types";
 
 export const siteConfig: SiteConfig = {
 	author: "David Culemann",
-	name: "easycv",
-	project: "Easycv",
-	description: "Supercharge your career with AI.",
+	name: "corpify",
+	project: "Corpify",
+	description: "Innovate, Disrupt, and Scale with Our All-In-One Solution.",
 	url: "https://www.davidculemann.com",
 	contactEmail: "davidculemann@gmail.com",
 	ogImage: "https://www.davidculemann.com/og.png",
 	links: {
-		twitter: "https://twitter.com/easycv",
+		twitter: "https://twitter.com/corpify",
 		github: "https://github.com/davidculemann",
 	},
 	github: {
