5ad7299ca84f622b4cc6ee25307190185fa5f8a9
davidculemann <davidculemann@gmail.com>
2024-11-04 21:15:05 +0000

    add dummy profile page

---
+++ app/routes/_app.account.settings.tsx
@@ -0,0 +1,19 @@
+import { getSupabaseWithHeaders } from "@/lib/supabase/supabase.server";
+import type { LoaderFunctionArgs } from "@remix-run/node";
+import { useLoaderData } from "@remix-run/react";
+
+export async function loader({ request }: LoaderFunctionArgs) {
+	const { supabase } = getSupabaseWithHeaders({ request });
+
+	const {
+		data: { user },
+	} = await supabase.auth.getUser();
+
+	return user;
+}
+
+export default function Settings() {
+	const user = useLoaderData<typeof loader>();
+
+	return <div className="flex flex-col pt-4"></div>;
+}
