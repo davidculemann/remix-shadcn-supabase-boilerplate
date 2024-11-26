78abc668a899f96cb3df7995a7cc11b7821d4144
David Culemann <davidculemann@gmail.com>
2024-11-26 15:37:17 +0000

    fetch profile and subscription  on the app level and pass down through Outlet

--- app/routes/_app.tsx
+++ app/routes/_app.tsx
@@ -11,13 +11,54 @@ import {
 } from "@/components/ui/breadcrumb";
 import { useCurrentPage } from "@/hooks/use-current-page";
 import type { SupabaseOutletContext } from "@/lib/supabase/supabase";
-import { Link, Outlet, useOutletContext } from "@remix-run/react";
+import { getSupabaseWithHeaders } from "@/lib/supabase/supabase.server";
+import { Link, Outlet, useLoaderData, useNavigate, useOutletContext } from "@remix-run/react";
+import { json, redirect } from "@remix-run/node";
 import { Fragment } from "react";
+import type { LoaderFunctionArgs } from "@remix-run/node";
+import { getLocaleCurrency } from "@/services/stripe/stripe.server";
+import { useToast } from "@/components/hooks/use-toast";
+
+export async function loader({ request }: LoaderFunctionArgs) {
+	const { supabase } = getSupabaseWithHeaders({ request });
+	const {
+		data: { user },
+		error,
+	} = await supabase.auth.getUser();
+
+	if (error || !user) throw redirect("/signin");
+
+	const { data: profile, error: profileError } = await supabase
+		.from("profiles")
+		.select("*")
+		.eq("id", user.id)
+		.single();
+	const { data: subscription } = await supabase.from("subscriptions").select("*").eq("user_id", user.id).single();
+
+	const currency = getLocaleCurrency(request);
+
+	if (profileError || !profile)
+		return json({ message: profileError?.message || "Could not get profile" }, { status: 400 });
+
+	return { profile, subscription, currency };
+}
 
 export default function AuthLayout() {
+	const loaderData = useLoaderData<typeof loader>();
 	const { supabase, isLoading, user } = useOutletContext<SupabaseOutletContext>();
-	//NOTE: here we should be fetching the profile and planId from the database
 	const { breadcrumbs, activePage } = useCurrentPage();
+	const { toast } = useToast();
+	const navigate = useNavigate();
+
+	if ("message" in loaderData) {
+		toast({
+			variant: "destructive",
+			description: loaderData.message,
+		});
+		return navigate("/signin");
+	}
+
+	const { profile, subscription, currency } = loaderData;
 
 	if (isLoading || !user) {
 		return <PageLoading />;
@@ -49,7 +90,7 @@ export default function AuthLayout() {
 						))}
 					</BreadcrumbList>
 				</Breadcrumb>
-				<Outlet context={{ supabase }} />
+				<Outlet context={{ supabase, profile, subscription, currency }} />
 			</ContentLayout>
 		</AdminPanelLayout>
 	);
