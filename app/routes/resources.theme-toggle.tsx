074485dc25d3b3d5a7544091fcf3c476dabbe25b
David Culemann <davidculemann@gmail.com>
2024-11-30 16:32:14 +0000

    improved theme toggle

--- app/routes/resources.theme-toggle.tsx
+++ app/routes/resources.theme-toggle.tsx
@@ -1,16 +1,12 @@
-import { Moon, Sun } from "lucide-react";
-
+import { MoonIcon, SunIcon } from "lucide-react";
 import { Button } from "@/components/ui/button";
-import {
-	DropdownMenu,
-	DropdownMenuContent,
-	DropdownMenuItem,
-	DropdownMenuTrigger,
-} from "@/components/ui/dropdown-menu";
 import { setTheme } from "@/lib/theme.server";
 import type { ActionFunction } from "@remix-run/node";
 import { json } from "@remix-run/node";
 import { useFetcher, useFetchers } from "@remix-run/react";
+import { TooltipContent, TooltipProvider, TooltipTrigger } from "@/components/ui/tooltip";
+import { Tooltip } from "@/components/ui/tooltip";
+import { useTheme } from "@/lib/client-hints";
 
 enum Theme {
 	DARK = "dark",
@@ -49,7 +45,6 @@ export const action: ActionFunction = async ({ request }) => {
 export function useOptimisticTheme(): Theme | null {
 	const fetchers = useFetchers();
 	const themeFetcher = fetchers.find((f) => f.formAction === "/resources/theme-toggle");
-
 	const optimisticTheme = themeFetcher?.formData?.get("theme");
 
 	if (optimisticTheme && isTheme(optimisticTheme)) {
@@ -61,25 +56,36 @@ export function useOptimisticTheme(): Theme | null {
 
 export function ThemeToggle() {
 	const fetcher = useFetcher();
+	const theme = useTheme();
 
-	const handleThemeChange = (theme: Theme) => {
-		fetcher.submit({ theme }, { method: "post", action: "/resources/theme-toggle" });
+	const handleThemeChange = () => {
+		const newTheme = theme === "dark" ? "light" : "dark";
+		fetcher.submit(
+			{ theme: newTheme },
+			{
+				method: "post",
+				action: "/resources/theme-toggle",
+			},
+		);
 	};
 
 	return (
-		<DropdownMenu>
-			<DropdownMenuTrigger asChild>
-				<Button variant="outline" size="icon">
-					<Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
-					<Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
-					<span className="sr-only">Toggle theme</span>
-				</Button>
-			</DropdownMenuTrigger>
-			<DropdownMenuContent align="end">
-				<DropdownMenuItem onClick={() => handleThemeChange(Theme.LIGHT)}>Light</DropdownMenuItem>
-				<DropdownMenuItem onClick={() => handleThemeChange(Theme.DARK)}>Dark</DropdownMenuItem>
-				<DropdownMenuItem onClick={() => handleThemeChange(Theme.SYSTEM)}>System</DropdownMenuItem>
-			</DropdownMenuContent>
-		</DropdownMenu>
+		<TooltipProvider disableHoverableContent>
+			<Tooltip delayDuration={100}>
+				<TooltipTrigger asChild>
+					<Button
+						className="rounded-full w-8 h-8 bg-background mr-2"
+						variant="outline"
+						size="icon"
+						onClick={() => handleThemeChange()}
+					>
+						<SunIcon className="w-[1.2rem] h-[1.2rem] rotate-90 scale-0 transition-transform ease-in-out duration-500 dark:rotate-0 dark:scale-100" />
+						<MoonIcon className="absolute w-[1.2rem] h-[1.2rem] rotate-0 scale-1000 transition-transform ease-in-out duration-500 dark:-rotate-90 dark:scale-0" />
+						<span className="sr-only">Switch Theme</span>
+					</Button>
+				</TooltipTrigger>
+				<TooltipContent side="bottom">Switch Theme</TooltipContent>
+			</Tooltip>
+		</TooltipProvider>
 	);
 }
