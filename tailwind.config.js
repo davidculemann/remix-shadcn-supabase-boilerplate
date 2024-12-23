414acb86963a741099403f8ee54e12c261cbd747
David Culemann <davidculemann@gmail.com>
2024-12-23 21:16:08 +0400

    fix require statement

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,5 @@
+import animate from "tailwindcss-animate";
+
 /** @type {import('tailwindcss').Config} */
 export default {
 	darkMode: ["class"],
@@ -134,5 +136,5 @@ export default {
 			},
 		},
 	},
-	plugins: [require("tailwindcss-animate")],
+	plugins: [animate],
 };
